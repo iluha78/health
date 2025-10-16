ceb7893edd003ccde51daa88c70f28f186c62b6f
iluha78 <tarasov.ilya@gmail.com>
2025-10-16 20:50:42 +0200

    Обработать CORS и предварительные запросы

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,6 +1,7 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -9,6 +10,38 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
+
+$corsRaw = $_ENV['CORS_ALLOWED_ORIGINS'] ?? '*';
+$allowedOrigins = array_values(array_filter(array_map('trim', explode(',', $corsRaw))));
+
+$app->add(function (Request $request, RequestHandler $handler) use ($allowedOrigins, $app): Response {
+    $isPreflight = strtoupper($request->getMethod()) === 'OPTIONS';
+    $response = $isPreflight ? $app->getResponseFactory()->createResponse(204) : $handler->handle($request);
+
+    $origin = $request->getHeaderLine('Origin');
+    $allowOrigin = null;
+
+    if (count($allowedOrigins) === 0 || ($allowedOrigins[0] === '*' && count($allowedOrigins) === 1)) {
+        $allowOrigin = '*';
+    } elseif ($origin !== '' && in_array($origin, $allowedOrigins, true)) {
+        $allowOrigin = $origin;
+    }
+
+    if ($allowOrigin !== null) {
+        $response = $response
+            ->withHeader('Access-Control-Allow-Origin', $allowOrigin)
+            ->withHeader('Vary', 'Origin');
+
+        if ($allowOrigin !== '*') {
+            $response = $response->withHeader('Access-Control-Allow-Credentials', 'true');
+        }
+    }
+
+    return $response
+        ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        ->withHeader('Access-Control-Max-Age', '86400');
+});
 
 require __DIR__ . '/../config/db.php';
 require __DIR__ . '/../config/routes.php';
