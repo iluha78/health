078d3568059e6805782daff8661bd7e378c959ed
iluha78 <tarasov.ilya@gmail.com>
2025-11-05 22:34:43 +0100

    Group pressure routes to avoid 404

--- backend/config/routes.php
+++ backend/config/routes.php
@@ -12,6 +12,7 @@ use App\Controllers\AnalysisController;
 use App\Controllers\AssistantController;
 use App\Controllers\PressureController;
 use App\Middleware\JwtMiddleware;
+use Slim\Routing\RouteCollectorProxy;
 
 /** @var App $app */
 $app->post('/auth/register', [AuthController::class, 'register']);
@@ -25,8 +26,12 @@ $app->get('/lipids',    [LipidController::class, 'list'])->add(new JwtMiddleware
 $app->post('/lipids',   [LipidController::class, 'create'])->add(new JwtMiddleware());
 $app->delete('/lipids/{id}',[LipidController::class,'delete'])->add(new JwtMiddleware());
 
-$app->get('/pressure',  [PressureController::class, 'list'])->add(new JwtMiddleware());
-$app->post('/pressure', [PressureController::class, 'create'])->add(new JwtMiddleware());
+$app->group('/pressure', function (RouteCollectorProxy $group): void {
+    $group->get('',  [PressureController::class, 'list']);
+    $group->get('/', [PressureController::class, 'list']);
+    $group->post('', [PressureController::class, 'create']);
+    $group->post('/', [PressureController::class, 'create']);
+})->add(new JwtMiddleware());
 
 $app->get('/diary/{date}',              [DiaryController::class, 'getDay'])->add(new JwtMiddleware());
 $app->post('/diary/{date}/items',       [DiaryController::class, 'addItem'])->add(new JwtMiddleware());
